16510d17e92deabd0cd882599d122ad24a1cc888
Sacses Og <103183316+SacsesOg@users.noreply.github.com>
2022-09-16 11:38:32 +0200

    ES UN EXPERIMENTO DE ANIMACION

---
+++ js/animacion.js
@@ -0,0 +1,14 @@
+lottie.loadAnimation({
+    container: document.getElementById('lottie'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json', // the path to the animation json
+  });
+  lottie.loadAnimation({
+    container: document.getElementById('lottie2'), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data2.json', // the path to the animation json
+  });
